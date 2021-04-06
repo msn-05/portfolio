37c887ad292ad669c6a460330986be621f861e46
msn-05 <saadmannuheen2005@gmail.com>
2021-04-06 20:15:13 +0600

    Upgraded a lotta stuff

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 var pBars = document.getElementsByTagName('progress');
 pBars = Array.prototype.slice.call(pBars);
-var widths = [90, 70, 60, 40, 15, 10];
+var widths = [90, 70, 60, 40, 15, 40];
 window.onload = () => {
   for (let i in pBars){
     pBars[i].value = widths[i];
+  }
+  let a = document.querySelectorAll('a');
+  for (let e of a){
+    e.target = "_blank";
   }
 };
 function filter(txt,button){
@@ -19,10 +23,16 @@ function filter(txt,button){
   for (let el of projects){
     let tags = el.dataset.tags.split(',');
     if (tags.includes(txt)){
-      el.style.display = "flex";
+      el.classList.remove("project-hide");
+      setTimeout(()=>{
+        el.style.display = "flex";
+      },300);
     }
     else{
-      el.style.display = "none";
+      el.classList.add("project-hide");
+      setTimeout(()=>{
+        el.style.display = "none";
+      },300);
     }
   }
 }
